27fe4072781ad1cb0fc2b6ed1b9c28bd8f19a650
listening <893979653@qq.com>
2018-02-27 14:57:12 +0800

    修改spark路径

--- WordCount.sh
+++ WordCount.sh
@@ -1,1 +1,1 @@
-/opt/spark-1.6.0/bin/spark-submit --class com.lorelib.spark.WordCountOnCluster --master spark://master:7077 /opt/data/SparkInAction-1.0.jar
+/usr/local/spark/spark-1.6.0/bin/spark-submit --class com.lorelib.spark.WordCountOnCluster --master spark://master:7077 /opt/data/SparkInAction-1.0.jar
